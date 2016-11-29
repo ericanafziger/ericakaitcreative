0fa15bc19f8146a65f4d9426dd415210416708b7
Erica Nafziger <ericakait@Ericas-iMac.local>
2016-11-28 20:30:21 -0800

    create slideshow with jquery - left and right arrows work - dots below do not

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,35 @@
 $(document).ready(function(){
+  var totalImages = $("#images").children().length; // number of total slideshow images
+  var imageCounter = $("#images").children().length; // keeps track of current image
+  var startofImages = 1;
+  $("#arrows .fa-angle-left").click(function(){
+    var photo = parseInt($("#images .active").attr('class')); // grabs active photo
+    $("#images img").removeClass("active");
+    if (photo===1) {
+      imageCounter = $("#images").children().length;
+      $("#images img:nth-child("+imageCounter+")").addClass("active");
+    } else {
+      imageCounter -= 1;
+      startofImages = imageCounter;
+      $("#images img:nth-child("+imageCounter+")").addClass("active");
+    }
+    // alert("image counter is equal to " +imageCounter);
 
+  });
+  $("#arrows .fa-angle-right").click(function(){
+    var photo = parseInt($("#images .active").attr('class')); // grabs active photo
+    $("#images img").removeClass("active");
+    if (photo < imageCounter) {
+      imageCounter = photo + 1;
+      $("#images img:nth-child("+imageCounter+")").addClass("active");
+    } else if (photo === totalImages) {
+      imageCounter = 1;
+      $("#images img:nth-child("+imageCounter+")").addClass("active");
+    } else {
+      imageCounter += 1;
+      $("#images img:nth-child("+imageCounter+")").addClass("active");
+    }
+    // alert("image counter is equal to " +imageCounter);
 
-
+  });
 }); //end of document ready
